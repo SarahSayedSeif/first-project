6a478395d549a3a64d1369704ea4171c88b0994a
SarahSayedSeif <108439028+SarahSayedSeif@users.noreply.github.com>
2022-07-02 18:02:37 -0700

    Update javaScript.js

--- JS-demo/js/javaScript.js
+++ JS-demo/js/javaScript.js
@@ -3,8 +3,9 @@ var productName = document.getElementById('productName');
 var productPrice = document.getElementById('productPrice');
 var productCategory = document.getElementById('productCategory');
 var productDesc = document.getElementById('productDesc');
-
 var tableBody = document.getElementById('tableBody');
+var addBtn = document.getElementById('addBtn');
+var updateBtn = document.getElementById('updateBtn');
 
 var productContainer ;
 
@@ -19,19 +20,27 @@ else
     productContainer = [];
 }
 
-// // localStorage.setItem("userName","Ali")
 
-function addProduct() {
-    var product= {
-        name: productName.value,
-        price: productPrice.value,
-        category: productCategory.value,
-        desc: productDesc.value
-     }
-    productContainer.push(product)
-    localStorage.setItem('myProuducts',JSON.stringify(productContainer) ) 
+function addProducts() {
+    
+    if(validateProductName()== true && validateProductPrice()== true)
+    {
+        var product = {
+        name: productNameInput.value,
+        price: productPriceInput.value,
+        category: productCategoryInput.value,
+        description: productDescriptionInput.value
+    }
+    productContainer.push(product);
+    localStorage.setItem('myProducts' ,JSON.stringify(productContainer))
     clearForm();
     displayProducts(productContainer);
+    }
+    else
+    {
+        alert('productName invalid')
+    }
+
 }
 
 // // to save date in localStorage to keep it there
@@ -57,8 +66,8 @@ function displayProducts(productList)
         <td>${productList[i].price}</td>
         <td>${productList[i].category}</td>
         <td>${productList[i].desc}</td>
-        <td><button class="btn btn-sm btn-outline-warning">update</button></td>
-        <td><button class="btn btn-sm btn-outline-danger">delete</button></td>
+        <td><button onclick="updatedProducts(${i})" class="btn  btn-sm btn-outline-warning">update</button></td>
+        <td><button onclick="deletedProducts(${i})"  class="btn  btn-sm btn-outline-danger">delete</button></td>
       
         </tr>`
     }
@@ -81,10 +90,76 @@ function searchProduct(searchTerm)
     displayProducts(searchResult);
 }
 
-function deletedProduct(deletedIndex) {
+
+function deletedProducts(deletedIndex) {
     productContainer.splice(deletedIndex, 1);
+    localStorage.setItem('myProducts' ,JSON.stringify(productContainer))
+
     displayProducts(productContainer);
 }
 
-deletedProduct(1);
+function updatedProducts(updatedIndex) {
+
+    productNameInput.value = productContainer[updatedIndex].name;
+    productPriceInput.value = productContainer[updatedIndex].price;
+    productCategoryInput.value = productContainer[updatedIndex].category;
+    productDescriptionInput.value = productContainer[updatedIndex].description;
+
+    addBtn.innerHTML= 'upDate'
+}
+
+
+var list = 0;
+
+function edit() {
+
+    productContainer[list].name = productNameInput.value,
+    productContainer[list].price = productPriceInput.value,
+    productContainer[list].category = productCategoryInput.value,
+    productContainer[list].description = productDescriptionInput.value,
+    
+        localStorage.setItem('myProducts', JSON.stringify(productContainer))
+        displayProducts(productContainer)
+
+}
+
+
+function add() {
+    if (addBtn.innerHTML == 'upDate') {
+
+        edit();
+        clearForm();
+    }
+    else {
+        addProducts();
+    }
+}
+
+function validateProductName() {
+    
+    var regexa = /^[A-Z][a-z]{3,8}$/;
+    if (regexa.test(productNameInput.value) == true) {
+        productNameInput.classList.replace('is-invalid' , 'is-valid');
+        return true;
+    }
+    else {
+        productNameInput.classList.add('is-invalid')
+        return false;
+    }
+}
+
+
+function validateProductPrice() {
+    
+    var regex = /^[0-9]{1,5}$/;
+    if (regex.test(productPriceInput.value) == true)
+    {
+        productPriceInput.classList.replace('is-invalid' , 'is-valid');
+        return true;
+    }
+    else {
+        productPriceInput.classList.add('is-invalid')
+        return false;
+    }
+}
 
